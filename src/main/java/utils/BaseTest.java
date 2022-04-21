3d19d73dcc87fdec05c93ca28597c5a651585a95
Andreea Birna <andreea.birna@yahoo.com>
2022-04-21 21:11:52 +0300

    modified base

--- src/main/java/utils/BaseTest.java
+++ src/main/java/utils/BaseTest.java
@@ -7,10 +7,15 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxBinary;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
@@ -23,16 +28,54 @@ public class BaseTest {
 	
   public WebDriver driver;
   
+    @Parameters({"browser"})
 	@BeforeClass
-	public void setUp() throws MalformedURLException {
+	public void setUp(String browser) throws MalformedURLException {
 		//System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
 		
-		driver =  new ChromeDriver();	
+		ChromeOptions option = new ChromeOptions();
+		option.addArguments("--headless");
+		option.addArguments("start-maximized");
+		
+		FirefoxBinary firefoxBinary = new FirefoxBinary();
+		firefoxBinary.addCommandLineOptions("--headless");
+		FirefoxOptions foption = new FirefoxOptions();
+		foption.setBinary(firefoxBinary);
+		
+		if(browser != "" && browser != null) {
+			
+			if(browser.equalsIgnoreCase("chrome")) {
+				driver =  new ChromeDriver(option);	
+
+			}else if(browser.equalsIgnoreCase("firefox")) {
+				driver =  new FirefoxDriver(foption);
+
+			}
+				
+		}else {
+			
+			driver =  new ChromeDriver(option);	
+
+			
+		}
+		
+		
+		
+		
+		
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		driver.manage().window().setSize(new Dimension(1440, 900));
+		
+		driver.get("https://keybooks.ro/");
+		
+		//driver =  new ChromeDriver();	
+		//driver.manage().window().maximize();
+	//	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	
 		//driver =initDriver(browser);
-		driver.get("https://keybooks.ro/");
+	//	driver.get("https://keybooks.ro/");
 		//driver.get("https://the-internet.herokuapp.com/javascript_alerts");
 		//driver.get("https://the-internet.herokuapp.com/dynamic_loading/1");
 	}
